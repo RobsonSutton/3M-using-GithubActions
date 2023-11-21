4faced9d2f2fe7bca482fe8754cd6640dea7a270
Robson Sutton <robson.sutton@nationwide.co.uk>
2023-11-21 15:11:18 +0000

    update trigger

--- main.tf
+++ main.tf
@@ -12,6 +12,6 @@ terraform {
 # An example resource that does nothing.
 resource "null_resource" "example" {
   triggers = {
-    value = "A example resource that does nothing!"
+    value = "This example resource does nothing!"
   }
 }
